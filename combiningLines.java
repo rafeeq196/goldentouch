5c8dce775c6926dd92f7204b9de28fff4b37d9fe
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-24 19:12:01 +0530

    Create combiningLines.java

---
+++ combiningLines.java
@@ -0,0 +1,19 @@
+package Exercise;
+
+public class combiningLines {
+	public static void main(String[]args) {
+		String a = ("I");
+		String b = ("am");
+		String c = ("learning");
+		String d = ("java") ;
+		String e = ("now");
+		String f = ("a days");
+		String g = ("by");
+		String h = ("Shoib Sir");
+		String fullStatement = a +" "+ b +" "+ c +" "+ d +" "+ e +" "+ f+" "+ g +" "+h;
+		System.out.println(fullStatement);
+		
+	}
+	
+
+}
